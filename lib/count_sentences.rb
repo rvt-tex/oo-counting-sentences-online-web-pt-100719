59442b9dd4408c09567fde13cc861cf47d2b3723
Ruel Tingling <rvtingling@gmail.com>
2019-10-21 20:18:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
     else
       return false
     end
-  end
+  
 
   def exclamation?
     if self.end_with?("!")
@@ -23,9 +23,11 @@ class String
     else
       return false
     end
-  end
+  
 
   def count_sentences
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
     return new_array.length
   end
+  
+end 
