3ebe93a3218b4447de1906eaebe6da1972626a53
Ruel Tingling <rvtingling@gmail.com>
2019-10-21 20:16:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -8,20 +8,25 @@ class String
     else
       return false
   end
-  
-
-    
-  end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
